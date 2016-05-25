d608444169ad9968880ab945e5a616b49f9d042d
jordanhatch82 <jordan@jordanhatch.com>
2016-05-24 20:15:47 -0700

    Update conn.cfg.php

--- app/Vendor/conn.cfg.php
+++ app/Vendor/conn.cfg.php
@@ -1,5 +1,5 @@
 <?php
 
-$connInfo = array('tt135:tt135:i:924bea69f2a29b59a1ceca29706270ca:This is the connection for applicationName.infusionsoft.com');
+$connInfo = array('tt135:tt135:i:1234134gqerg:This is the connection for applicationName.infusionsoft.com');
 
 ?>
